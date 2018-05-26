0fe4e8335ae10b054e6b3fa01913550c6e9c644d
Ken M. Lai <ken.mercus.lai@gmail.com>
2018-05-26 16:45:53 +0800

    feat: add module to calculate distance of given two geolocation pairs
    # |<----  Using a Maximum Of 50 Characters  ---->|
    
    
    # Explain why this change is being made
    # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
    
    # Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23
    
    # --- COMMIT END ---
    # Type can be 
    #    feat (new feature)
    #    fix (bug fix)
    #    docs (changes to documentation)
    #    style (formatting, missing semi colons, etc; no code change)
    #    refactor (refactoring production code)
    #    test (adding missing tests, refactoring tests; no production code change)
    #    chore (updating grunt tasks etc; no production code change)
    # --------------------
    # Remember to
    #    Separate subject from body with a blank line
    #    Limit the subject line to 50 characters
    #    Capitalize the subject line
    #    Do not end the subject line with a period
    #    Use the imperative mood in the subject line
    #    Wrap the body at 72 characters
    #    Use the body to explain what and why vs. how
    #    Can use multiple lines with "-" for bullet points in body
    # --------------------
    # For more information about this template, check out
    # https://gist.github.com/adeekshith/cd4c95a064977cdc6c50

---
+++ flightaware2columbus/tests/test_geo_distance.py
@@ -0,0 +1,32 @@
+from flightaware2columbus.geo_distance import hav, get_distance_hav
+
+
+def test_hav():
+    assert hav(0) == 0
+    assert round(hav(20), 5) == round((-0.5440211108893699) ** 2, 5)
+
+
+def test_get_distance_hav():
+    lon1, lat1 = (22.599578, 113.973129)  # 深圳野生动物园(起点）
+    lon2, lat2 = (22.6986848, 114.3311032)  # 深圳坪山站 (百度地图测距：38.3km)
+    d2 = get_distance_hav(lon1, lat1, lon2, lat2)
+    known_distance = 38.3
+    assert known_distance * 0.99 <= d2 <= known_distance * 1.01
+
+    lon2, lat2 = (39.9087202, 116.3974799)  # 北京天安门(1938.4KM)
+    d2 = get_distance_hav(lon1, lat1, lon2, lat2)
+    known_distance = 1938.4
+    assert known_distance * 0.99 <= d2 <= known_distance * 1.01
+
+    lon2, lat2 = (34.0522342, -118.2436849)  # 洛杉矶(11625.7KM)
+    d2 = get_distance_hav(lon1, lat1, lon2, lat2)
+    known_distance = 11625.7
+    assert known_distance * 0.99 <= d2 <= known_distance * 1.01
+
+    lon11, lat11 = (22.599578, 114.973129)
+    lon21, lat21 = (34.0522342, -117.2436849)
+    assert get_distance_hav(lon1, lat1, lon2, lat2) == get_distance_hav(lon11, lat11, lon21, lat21)
+
+    lon11, lat11 = (-22.599578, 114.973129)
+    lon21, lat21 = (-34.0522342, -117.2436849)
+    assert get_distance_hav(lon1, lat1, lon2, lat2) == get_distance_hav(lon11, lat11, lon21, lat21)
