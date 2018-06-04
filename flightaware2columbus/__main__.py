1a3a679a6c80390e9ac0e60a1bc5c7bd188c8db1
Ken M. Lai <ken.mercus.lai@gmail.com>
2018-06-04 22:11:43 +0800

    feat(cli): add cli command to parse columbus csv file to generate GPX
    use --csv option to specify a csv file and --output for the gpx file

--- flightaware2columbus/__main__.py
+++ flightaware2columbus/__main__.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python
-
-"""Sample package entry point."""
-
-# import sys
+import click
+import gpx_writer, parsers
 
 
-# def main():
-#     sys.stdout.write("Hello, world!\n")
+@click.group()
+def main():
+    pass
 
 
-# if __name__ == '__main__':
-#     main()
+@click.command()
+@click.option('--csv', '-c', type=click.STRING, help='Columbus csv file')
+@click.option('--output', '-o', default=None, help='GPX file to write to')
+def columbus(csv, output):
+    track_points = parsers.read_columbus(csv)
+    description = parsers.desc_columbus(csv)
+    parsed_data = gpx_writer.xml_description(gpx_writer.xml_root(),
+                                             description)
+    parsed_data = gpx_writer.xml_trackpoints(parsed_data, track_points)
+    if output:
+        gpx_writer.write2xml(description,
+                             track_points,
+                             output)
+    else:
+        click.echo(parsed_data)
+
+
+# @click.command()
+# @click.option('--flight', '-f', help='Flight number')
+# @click.option('--output', '-o', default=None, help='GPX file to write to')
+# def parse_flight_route(flight, output):
+#     pass
+
+
+main.add_command(columbus)
+# main.add_command(parse_flight_route)
+
+if __name__ == '__main__':
+    main()
--- tests/test_all.py
+++ tests/test_all.py
@@ -1,17 +1,37 @@
-"""Sample integration test module."""
 # pylint: disable=no-self-use,missing-docstring
 
 # import unittest
+from unittest.mock import MagicMock
 
-# from IJCAI18 import sample
+# import click
+# from click.testing import CliRunner
+from flightaware2columbus.__main__ import columbus
+# from flightaware2columbus.parsers import read_columbus
 
 
-# class TestIjcai18(unittest.TestCase):
+def test_columnbus_echo():
+    read_columbus = MagicMock()
+    desc_columbus = MagicMock()
+    xml_description = MagicMock()
+    xml_trackpoints = MagicMock()
+    write2xml = MagicMock()
+    columbus('file.csv', None)
+    assert read_columbus.assert_called_once()
+    assert desc_columbus.assert_called_once()
+    assert xml_description.assert_called_once()
+    assert xml_trackpoints.assert_called_once()
+    assert write2xml.assert_not_called()
 
-#     """Sample integration test class."""
 
-#     def test_network_stuff(self):
-#         assert sample.function_with_network_stuff() is True
-
-#     def test_disk_stuff(self):
-#         assert sample.function_with_disk_stuff() is False
+def test_columnbus_to_gpx():
+    read_columbus = MagicMock()
+    desc_columbus = MagicMock()
+    xml_description = MagicMock()
+    xml_trackpoints = MagicMock()
+    write2xml = MagicMock()
+    columbus('file.csv', 'test.gpx')
+    assert read_columbus.assert_called_once()
+    assert desc_columbus.assert_called_once()
+    assert xml_description.assert_called_once()
+    assert xml_trackpoints.assert_called_once()
+    assert write2xml.assert_called_once()
