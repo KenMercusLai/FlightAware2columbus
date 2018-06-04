f5f6b235177c6a1a01a619f20c871c120842abe4
Ken M. Lai <ken.mercus.lai@gmail.com>
2018-06-04 22:08:03 +0800

    chore: add package path to fix issue when running unit tests

--- flightaware2columbus/__init__.py
+++ flightaware2columbus/__init__.py
@@ -1,4 +1,7 @@
 """Package for FlightAware2columbus."""
+import sys
+import os
+sys.path.append(os.path.join(os.getcwd(), 'flightaware2columbus'))
 
 __project__ = 'flightaware2columbus'
 __version__ = '0.0.0'
